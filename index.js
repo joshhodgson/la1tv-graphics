b4769c5953fd3ab7523d4652b74937bccaff8ce8
Josh Hodgson <josh.pbh@gmail.com>
2016-05-16 13:32:32 +0100

    Add socket relays

--- index.js
+++ index.js
@@ -6,7 +6,18 @@ var io = socket.listen(server);
 
 app.use(express.static('public'));
 
+var bug = false;
+var live = false;
+var clock = false;
+var ticker = false;
+var l3 = {"show": false};
+
 io.on('connect', function(socket) {
   console.log("Socket client connected");
+socket.emit('bug', bug);
+socket.emit('live', live);
+socket.emit('clock', clock);
+socket.emit('ticker', ticker);
+socket.emit('lowerthird', l3);
 
 })
