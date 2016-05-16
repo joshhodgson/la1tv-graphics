8696d1f6bda8ee8144fe55a3e0ab7138d6dc9b0f
Josh Hodgson <josh.pbh@gmail.com>
2016-05-16 13:33:10 +0100

    add socket functionality

--- public/cg.js
+++ public/cg.js
@@ -24,3 +24,68 @@ window.onload = function() {
   GetClock();
   setInterval(GetClock, 1000);
 }
+
+var socket = io.connect('http://' + window.location.host);
+
+socket.on('connect', function(socket) {
+  console.log("Connected!")
+})
+
+
+socket.on('live', function(payload) {
+  if (payload == true) {
+    $("#livecontainer").addClass("live");
+    $("#clockcontainer").addClass("live");
+
+  } else if (payload == false) {
+    $("#livecontainer").removeClass("live");
+    $("#clockcontainer").removeClass("live");
+
+  } else {
+    console.log("Error - don't know what to do for live bug with " + payload)
+  }
+})
+
+socket.on('time', function(payload) {
+  if (payload == true) {
+    $("#clockcontainer").addClass("time");
+
+  } else if (payload == false) {
+    $("#clockcontainer").removeClass("time");
+
+  } else {
+    console.log("Error - don't know what to do for time bug with " + payload)
+  }
+})
+socket.on('bug', function(payload) {
+  if (payload == true) {
+    $("#logobox").addClass("fadeInRight").removeClass("fadeOutRight");
+
+  } else if (payload == false) {
+    $("#logobox").addClass("fadeOutRight").removeClass("fadeInRight");
+
+  } else {
+    console.log("Error - don't know what to do for logo bug with " + payload)
+  }
+})
+
+socket.on('lowerThird', function(payload) {
+  if (payload.show == true) {
+    if ($('#lowerthird').hasClass('fadeInLeft')) {
+      $('#lowerthird').addClass('fadeOutLeft');
+      setTimeout(function() {
+        $(".lowerthirdtext").text(payload.line1);
+        $(".lowerthirdsecondline").text(payload.line2);
+        $('#lowerthird').addClass('fadeInLeft').removeClass('fadeOutLeft')
+      }, 1000)
+    } else {
+      $(".lowerthirdtext").text(payload.line1);
+      $(".lowerthirdsecondline").text(payload.line2);
+      $('#lowerthird').addClass('fadeInLeft').removeClass('fadeOutLeft');
+    }
+  } else if (payload.show == false) {
+    $('#lowerthird').addClass('fadeOutLeft').removeClass('fadeInLeft')
+  } else {
+    console.log("Error - don't know what to do for l3 with " + payload)
+  }
+})
