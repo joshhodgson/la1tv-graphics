80f2c1c6aabf877e86ba109998c8684290010135
Josh Hodgson <josh.pbh@gmail.com>
2016-05-16 13:58:56 +0100

    fix socket issue
    sockets going back to sender, not to other page

--- index.js
+++ index.js
@@ -10,14 +10,32 @@ var bug = false;
 var live = false;
 var clock = false;
 var ticker = false;
-var l3 = {"show": false};
+var l3 = {
+  "show": false
+};
 
 io.on('connect', function(socket) {
   console.log("Socket client connected");
-socket.emit('bug', bug);
-socket.emit('live', live);
-socket.emit('clock', clock);
-socket.emit('ticker', ticker);
-socket.emit('lowerthird', l3);
+  socket.emit('bug', bug);
+  socket.emit('live', live);
+  socket.emit('clock', clock);
+  socket.emit('ticker', ticker);
+  socket.emit('lowerThird', l3);
+  socket.on('bug', function(payload) {
+    io.emit('bug', payload)
+  })
+  socket.on('live', function(payload) {
+    io.emit('live', payload)
+    console.log('emmited live - ' + payload)
+  })
+  socket.on('clock', function(payload) {
+    io.emit('clock', payload)
+  })
+  socket.on('ticker', function(payload) {
+    io.emit('ticker', payload)
+  })
+  socket.on('lowerThird', function(payload) {
+    io.emit('lowerThird', payload)
+  })
 
 })
