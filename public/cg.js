577cdba4f1450e6b87e62693a302014707aef91a
joshhodgson <josh.pbh@gmail.com>
2016-05-16 16:08:41 +0100

    Transition to JQuery marquee

--- public/cg.js
+++ public/cg.js
@@ -93,12 +93,45 @@ socket.on('lowerThird', function(payload) {
 
 socket.on('ticker', function(payload) {
   if (payload.show == true) {
-    $("#ticker").addClass("fadeInUp").removeClass("fadeOutDown");
+    if ($("#ticker").hasClass("slideInUp")) {
+      $("#ticker").addClass("slideOutDown").removeClass("slideInUp");
+      setTimeout(function() {
+        $("#tickertext").text(payload.text);
+        runCrawler($('#tickercontainer'), $('#tickertext'), false)
+        $("#ticker").addClass("slideInUp").removeClass("slideOutDown");
+      }, 1000)
+
+
+    } else {
+      $("#tickertext").text(payload.text);
+      runCrawler($('#tickercontainer'), $('#tickertext'))
+      $("#ticker").addClass("slideInUp").removeClass("slideOutDown");
+    }
 
   } else if (payload.show == false) {
-    $("#ticker").addClass("fadeOutDown").removeClass("fadeInUp");
+    $("#ticker").addClass("slideOutDown").removeClass("slideInUp");
 
   } else {
     console.log("Error - don't know what to do for ticker bug with " + payload)
   }
 })
+
+///////Begin ticker
+
+function runCrawler(container, text, loop) {
+  text.finish()
+  var containerWidth = container.width();
+  var textWidth = text.width();
+  console.log("conatiner text" + containerWidth + " " + textWidth)
+
+  var time = (textWidth + containerWidth) / .08
+  text.css("left", containerWidth)
+  text.animate({
+    left: -textWidth - 10
+  }, time, "linear", function() {
+    if ($("#ticker").hasClass("slideInUp")) {
+      text.css("left", containerWidth)
+      runCrawler($('#tickercontainer'), $('#tickertext'), true)
+    }
+  })
+}
