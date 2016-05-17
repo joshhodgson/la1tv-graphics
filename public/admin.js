ca6c3e028dca9fd80e6e0916ca4981432bf04696
Josh Hodgson <josh.pbh@gmail.com>
2016-05-17 21:36:23 +0100

    ticker enhancements

--- public/admin.js
+++ public/admin.js
@@ -1,76 +1,76 @@
 var socket = io.connect('http://' + window.location.host);
 
 socket.on('connect', function(socket) {
-    console.log("Connected!")
+  console.log("Connected!")
 })
 
 var lastL3 = 0
 
 function addLowerThird(line1, line2) {
-    console.log("New lower third! " + line1 + ", " + line2)
-    lastL3 = lastL3 + 1
-    $('#blankL3').clone().attr('id', "l3" + lastL3).insertBefore('#blankL3').removeClass('hidden')
-    var container = $('#l3' + lastL3)
-    container.find('.line1').text(line1)
-    container.find('.line2').text(line2)
+  console.log("New lower third! " + line1 + ", " + line2)
+  lastL3 = lastL3 + 1
+  $('#blankL3').clone().attr('id', "l3" + lastL3).insertBefore('#blankL3').removeClass('hidden')
+  var container = $('#l3' + lastL3)
+  container.find('.line1').text(line1)
+  container.find('.line2').text(line2)
 
-    container.find('.delete').click(function() {
-        container.remove()
+  container.find('.delete').click(function() {
+    container.remove()
+  })
+  container.find('.left').click(function() {
+    socket.emit('lowerThird', {
+      'show': true,
+      'side': 'left',
+      'line1': line1,
+      'line2': line2
     })
-    container.find('.left').click(function() {
-        socket.emit('lowerThird', {
-            'show': true,
-            'side': 'left',
-            'line1': line1,
-            'line2': line2
-        })
+  })
+  container.find('.right').click(function() {
+    socket.emit('lowerThird', {
+      'show': true,
+      'side': 'right',
+      'line1': line1,
+      'line2': line2
     })
-    container.find('.right').click(function() {
-        socket.emit('lowerThird', {
-            'show': true,
-            'side': 'right',
-            'line1': line1,
-            'line2': line2
-        })
-        console.log(line1)
-    })
+    console.log(line1)
+  })
 
-    container.find('.down').click(function() {
-        console.log('button clicked')
-        container.insertAfter(container.next())
-    })
+  container.find('.down').click(function() {
+    console.log('button clicked')
+    container.insertAfter(container.next())
+  })
 
-    container.find('.up').click(function() {
-        console.log('button clicked')
-        container.insertBefore(container.prev())
-    })
+  container.find('.up').click(function() {
+    console.log('button clicked')
+    container.insertBefore(container.prev())
+  })
 
 
 }
 var lastT = 0;
 
 function addTickerItem(text) {
-    console.log("New ticker item " + text)
-    lastT = lastT + 1
-    $('#blankT').clone().attr('id', "T" + lastT).insertBefore('#blankT').removeClass('hidden')
-    var container = $('#T' + lastT)
-    container.find('.line1').text(text)
+  console.log("New ticker item " + text)
+  lastT = lastT + 1
+  $('#blankT').clone().attr('id', "T" + lastT).insertBefore('#blankT').removeClass('hidden')
+  var container = $('#T' + lastT)
+  container.find('.line1').text(text)
 
-    container.find('.delete').click(function() {
-        container.remove()
-    })
+  container.find('.delete').click(function() {
+    container.remove()
+  })
 
-    container.find('.toggle').click(function() {
-        addTickerToList(text)
-    })
+  container.find('.toggle').click(function() {
+    addTickerToList(text)
+  })
 
 
 
 }
 
-function addTickerToList(text){
+function addTickerToList(text) {
   var item = $("<button type='button'></button>")
-  item.addClass("btn btn-success").text(text).click(function(){
+  item.addClass("btn btn-success tickerTextLi").text(text).click(function() {
     item.remove()
   })
   console.log(item.text())
@@ -79,7 +79,40 @@ function addTickerToList(text){
 
 }
 
-setTimeout(function() {
-    addLowerThird('hi', 'test');
-    addLowerThird('test', 'hello')
-}, 100)
+$(document).ready(function() {
+
+  $('#tickerShow').click(function() {
+    var text = ''
+    $(".tickerTextLi").each(function(index) {
+      text = text + $(this).text() + "  -  ";
+    });
+    text = text.substring(0, text.length - 5)
+    socket.emit('ticker', {
+      'show': true,
+      'text': text,
+      'loop': false
+    })
+  })
+  $('#tickerLoop').click(function() {
+    var text = ''
+    $(".tickerTextLi").each(function(index) {
+      text = text + $(this).text() + "  -  ";
+    });
+    text = text.substring(0, text.length - 5)
+    socket.emit('ticker', {
+      'show': true,
+      'text': text,
+      'loop': true
+    })
+  })
+  $('#tickerHide').click(function() {
+    socket.emit('ticker', {
+      show: false
+    })
+  })
+  addLowerThird('hi', 'test');
+  addLowerThird('test', 'hello')
+  addTickerItem('This is my name')
+  addTickerItem('This is a test')
+
+})
--- public/cg.js
+++ public/cg.js
@@ -1,162 +1,171 @@
 function GetClock() {
-    var d = new Date();
-    var nhour = d.getHours(),
-        nmin = d.getMinutes(),
-        ap;
-    if (nhour == 0) {
-        ap = "pm";
-        nhour = 12;
-    } else if (nhour < 12) {
-        ap = "pm";
-    } else if (nhour == 12) {
-        ap = "pm";
-    } else if (nhour > 12) {
-        ap = "pm";
-        nhour -= 12;
-    }
+  var d = new Date();
+  var nhour = d.getHours(),
+    nmin = d.getMinutes(),
+    ap;
+  if (nhour == 0) {
+    ap = "pm";
+    nhour = 12;
+  } else if (nhour < 12) {
+    ap = "pm";
+  } else if (nhour == 12) {
+    ap = "pm";
+  } else if (nhour > 12) {
+    ap = "pm";
+    nhour -= 12;
+  }
 
-    if (nmin <= 9) nmin = "0" + nmin;
+  if (nmin <= 9) nmin = "0" + nmin;
 
-    document.getElementById('clockbox').innerHTML = "" + nhour + ":" + nmin + ap + "";
+  document.getElementById('clockbox').innerHTML = "" + nhour + ":" + nmin + ap + "";
 }
 
 window.onload = function() {
-    GetClock();
-    setInterval(GetClock, 1000);
+  GetClock();
+  setInterval(GetClock, 1000);
 }
 
 var socket = io.connect('http://' + window.location.host);
 
 socket.on('connect', function(socket) {
-    console.log("Connected!")
+  console.log("Connected!")
 })
 
 
 socket.on('live', function(payload) {
-    console.log("live data - " + payload)
-    if (payload == true) {
-        $("#livecontainer").addClass("live");
-        $("#clockcontainer").addClass("live");
+  console.log("live data - " + payload)
+  if (payload == true) {
+    $("#livecontainer").addClass("live");
+    $("#clockcontainer").addClass("live");
 
-    } else if (payload == false) {
-        $("#livecontainer").removeClass("live");
-        $("#clockcontainer").removeClass("live");
+  } else if (payload == false) {
+    $("#livecontainer").removeClass("live");
+    $("#clockcontainer").removeClass("live");
 
-    } else {
-        console.log("Error - don't know what to do for live bug with " + payload)
-    }
+  } else {
+    console.log("Error - don't know what to do for live bug with " + payload)
+  }
 })
 
 socket.on('clock', function(payload) {
-    if (payload == true) {
-        $("#clockcontainer").addClass("time");
+  if (payload == true) {
+    $("#clockcontainer").addClass("time");
 
-    } else if (payload == false) {
-        $("#clockcontainer").removeClass("time");
+  } else if (payload == false) {
+    $("#clockcontainer").removeClass("time");
 
-    } else {
-        console.log("Error - don't know what to do for time bug with " + payload)
-    }
+  } else {
+    console.log("Error - don't know what to do for time bug with " + payload)
+  }
 })
 socket.on('bug', function(payload) {
-    if (payload == true) {
-        $("#logobox").addClass("fadeInRight").removeClass("fadeOutRight");
+  if (payload == true) {
+    $("#logobox").addClass("fadeInRight").removeClass("fadeOutRight");
 
-    } else if (payload == false) {
-        $("#logobox").addClass("fadeOutRight").removeClass("fadeInRight");
+  } else if (payload == false) {
+    $("#logobox").addClass("fadeOutRight").removeClass("fadeInRight");
 
+  } else {
+    console.log("Error - don't know what to do for logo bug with " + payload)
+  }
+})
+
+socket.on('lowerThird', function(payload) {
+  if (payload.side !== 'left') {
+    if (payload.show == true) {
+      if ($('#lowerthirdright').hasClass('fadeInRight')) {
+        $('#lowerthirdright').addClass('fadeOutRight');
+        setTimeout(function() {
+          $(".lowerthirdtextright").text(payload.line1);
+          $(".lowerthirdsecondlineright").text(payload.line2);
+          $('#lowerthirdright').addClass('fadeInRight').removeClass('fadeOutRight')
+        }, 1000)
+      } else {
+        $(".lowerthirdtextright").text(payload.line1);
+        $(".lowerthirdsecondlineright").text(payload.line2);
+        $('#lowerthirdright').addClass('fadeInRight').removeClass('fadeOutRight');
+      }
+    } else if (payload.show == false) {
+      $('#lowerthirdright').addClass('fadeOutRight').removeClass('fadeInRight')
     } else {
-        console.log("Error - don't know what to do for logo bug with " + payload)
+      console.log("Error - don't know what to do for l3 with " + payload)
     }
+  }
 })
 
 socket.on('lowerThird', function(payload) {
-    if (payload.side !== 'left') {
-        if (payload.show == true) {
-            if ($('#lowerthirdright').hasClass('fadeInRight')) {
-                $('#lowerthirdright').addClass('fadeOutRight');
-                setTimeout(function() {
-                    $(".lowerthirdtextright").text(payload.line1);
-                    $(".lowerthirdsecondlineright").text(payload.line2);
-                    $('#lowerthirdright').addClass('fadeInRight').removeClass('fadeOutRight')
-                }, 1000)
-            } else {
-                $(".lowerthirdtextright").text(payload.line1);
-                $(".lowerthirdsecondlineright").text(payload.line2);
-                $('#lowerthirdright').addClass('fadeInRight').removeClass('fadeOutRight');
-            }
-        } else if (payload.show == false) {
-            $('#lowerthirdright').addClass('fadeOutRight').removeClass('fadeInRight')
-        } else {
-            console.log("Error - don't know what to do for l3 with " + payload)
-        }
+  if (payload.side !== 'right') {
+    if (payload.show == true) {
+      if ($('#lowerthirdleft').hasClass('fadeInLeft')) {
+        $('#lowerthirdleft').addClass('fadeOutLeft');
+        setTimeout(function() {
+          $(".lowerthirdtextleft").text(payload.line1);
+          $(".lowerthirdsecondlineleft").text(payload.line2);
+          $('#lowerthirdleft').addClass('fadeInLeft').removeClass('fadeOutLeft')
+        }, 1000)
+      } else {
+        $(".lowerthirdtextleft").text(payload.line1);
+        $(".lowerthirdsecondlineleft").text(payload.line2);
+        $('#lowerthirdleft').addClass('fadeInLeft').removeClass('fadeOutLeft');
+      }
+    } else if (payload.show == false) {
+      $('#lowerthirdleft').addClass('fadeOutLeft').removeClass('fadeInLeft')
+    } else {
+      console.log("Error - don't know what to do for l3 with " + payload)
     }
-})
-
-socket.on('lowerThird', function(payload) {
-    if (payload.side !== 'right') {
-        if (payload.show == true) {
-            if ($('#lowerthirdleft').hasClass('fadeInLeft')) {
-                $('#lowerthirdleft').addClass('fadeOutLeft');
-                setTimeout(function() {
-                    $(".lowerthirdtextleft").text(payload.line1);
-                    $(".lowerthirdsecondlineleft").text(payload.line2);
-                    $('#lowerthirdleft').addClass('fadeInLeft').removeClass('fadeOutLeft')
-                }, 1000)
-            } else {
-                $(".lowerthirdtextleft").text(payload.line1);
-                $(".lowerthirdsecondlineleft").text(payload.line2);
-                $('#lowerthirdleft').addClass('fadeInLeft').removeClass('fadeOutLeft');
-            }
-        } else if (payload.show == false) {
-            $('#lowerthirdleft').addClass('fadeOutLeft').removeClass('fadeInLeft')
-        } else {
-            console.log("Error - don't know what to do for l3 with " + payload)
-        }
-    }
+  }
 })
 
 socket.on('ticker', function(payload) {
-    if (payload.show == true) {
-        if ($("#ticker").hasClass("slideInUp")) {
-            $("#ticker").addClass("slideOutDown").removeClass("slideInUp");
-            setTimeout(function() {
-                $("#tickertext").text(payload.text);
-                runCrawler($('#tickercontainer'), $('#tickertext'), false)
-                $("#ticker").addClass("slideInUp").removeClass("slideOutDown");
-            }, 1000)
+  if (payload.show == true) {
+    if ($("#ticker").hasClass("slideInUp")) {
+      $("#ticker").addClass("slideOutDown").removeClass("slideInUp");
+      setTimeout(function() {
+        $("#tickertext").text(payload.text);
+        runCrawler($('#tickercontainer'), $('#tickertext'), payload.loop)
+        $("#ticker").addClass("slideInUp").removeClass("slideOutDown");
+      }, 1000)
 
-
-        } else {
-            $("#tickertext").text(payload.text);
-            runCrawler($('#tickercontainer'), $('#tickertext'))
-            $("#ticker").addClass("slideInUp").removeClass("slideOutDown");
-        }
-
-    } else if (payload.show == false) {
-        $("#ticker").addClass("slideOutDown").removeClass("slideInUp");
 
     } else {
-        console.log("Error - don't know what to do for ticker bug with " + payload)
+      $("#tickertext").text(payload.text);
+      runCrawler($('#tickercontainer'), $('#tickertext'), payload.loop)
+      $("#ticker").addClass("slideInUp").removeClass("slideOutDown");
     }
+
+  } else if (payload.show == false) {
+    $("#ticker").addClass("slideOutDown").removeClass("slideInUp");
+
+  } else {
+    console.log("Error - don't know what to do for ticker bug with " + payload)
+  }
 })
 
 ///////Begin ticker
 
 function runCrawler(container, text, loop) {
-    text.finish()
-    var containerWidth = container.width();
-    var textWidth = text.width();
-    console.log("conatiner text" + containerWidth + " " + textWidth)
+  console.log(text)
+  text.finish()
+  var containerWidth = container.width();
+  var textWidth = text.width();
+  console.log("conatiner text" + containerWidth + " " + textWidth)
 
-    var time = (textWidth + containerWidth) / .08
-    text.css("left", containerWidth)
-    text.animate({
-        left: -textWidth - 10
-    }, time, "linear", function() {
-        if ($("#ticker").hasClass("slideInUp")) {
-            text.css("left", containerWidth)
-            runCrawler($('#tickercontainer'), $('#tickertext'), true)
+  var time = (textWidth + containerWidth) / .08
+  text.css("left", containerWidth)
+  text.animate({
+    left: -textWidth - 10
+  }, time, "linear", function() {
+    if ($("#ticker").hasClass("slideInUp")) {
+      if (typeof loop !== 'undefined') {
+        if (loop) {
+          text.css("left", containerWidth)
+          runCrawler($('#tickercontainer'), $('#tickertext'), true)
+        } else {
+          $("#ticker").addClass("slideOutDown").removeClass("slideInUp");
         }
-    })
+      } else {
+        $("#ticker").addClass("slideOutDown").removeClass("slideInUp");
+      }
+    }
+  })
 }
